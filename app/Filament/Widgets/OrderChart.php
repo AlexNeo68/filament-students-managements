943a1d5a48e35432535140276f94bfce8cbc2201
tunning_brain <tunning_brain@mail.ru>
2024-10-05 07:43:00 +0300

    orders chart

---
+++ app/Filament/Widgets/OrderChart.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Enums\OrderStatusEnum;
+use App\Models\Order;
+use App\Models\Product;
+use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
+
+class OrderChart extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+    protected static ?int $sort = 4;
+
+    protected function getData(): array
+    {
+        $data = Order::select('status', DB::raw('count(*) as count'))
+            ->groupBy('status')
+            ->pluck('count', 'status')
+            ->toArray();
+
+        $data2 = Product::get()->pluck('quantity')->toArray();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Orders',
+                    'data' => array_values($data)
+                ],
+                [
+                    'label' => 'Products count',
+                    'data' => array_values($data2),
+                    'backgroundColor' => '#36A2EB',
+                    'borderColor' => '#9BD0F5',
+                ]
+            ],
+            'labels' => OrderStatusEnum::cases()
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
